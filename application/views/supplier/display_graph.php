550db2c16b02a19fd1de41ff8517b56ce4236af8
Hafiz Bilal <hafiz.bilalahmadgondal@gmail.com>
2021-10-28 12:17:01 +0500

    merge conflicts fixes in display_grapgh w.r.t data

--- application/views/supplier/display_graph.php
+++ application/views/supplier/display_graph.php
@@ -1,15 +1,6 @@
 <?php $this->load->view('common/header.php');?>
-<!-- <script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/data.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js"></script> -->
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script> -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-<!-- <script src="https://unpkg.com/jspdf@latest/dist/jspdf.umd.min.js"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.4.1/jspdf.debug.js"></script>
 <style>
     .Charts {
         width: 100%;
@@ -19,7 +10,6 @@
 
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-12">
-        <!-- <input type="hidden" id="hidden_supplier_count" value="0" /> -->
         <div class="row">
             <div class="col-lg-12">
                 <h1>Guia de Fornecedores</h1>
@@ -56,12 +46,14 @@
                 <button class="btn btn-primary" id="btnPrint" onclick="PrintElem()" >Export To PDF</button>
             </div>
         </div>
-        <div class="row" id="contnet">
+        <div class="row" id="graph_content">
+
             <?php foreach ($materials as $key => $value) {?>
+                <?php if(isset($value['graph_data']) && !empty($value['graph_data'])){ ?>
 
-                <div id="<?= 'chartContainer'.$value['material_id']; ?>" style="height: 200px; width: 100%;margin-top:3%;"></div>
+                        <div id="<?= 'chartContainer'.$value['id']; ?>" style="height: 200px; width: 100%;margin-top:3%;"></div>
 
-            <?php } ?>
+            <?php } } ?>
 
         </div>
     </div>
@@ -70,19 +62,20 @@
 
 window.onload = function () {
     <?php foreach ($materials as $key => $value) {?>
-        var chart = new CanvasJS.Chart("<?= 'chartContainer'.$value['material_id'] ?>", {
+        var chart = new CanvasJS.Chart("<?= 'chartContainer'.$value['id'] ?>", {
     		title:{
-    			text: "<?= 'material '.$value['material_id'] ?>"
+    			text: "<?= $value['material_name'] ?>"
     		},
     		data: [
     		{
     			// Change type to "doughnut", "line", "splineArea", etc.
     			type: "column",
     			dataPoints: [
-                    <?php foreach ($value['supplier_data'] as $key1 => $supplier) {?>
-                        <?php if($supplier['count'] > 0){ ?>
-                        { label: "<?=$supplier['supplier_name'] ?>",  y: <?=$supplier['count'] ?>  },
-                    <?php } } ?>
+                    <?php if(isset($value['graph_data']) && !empty($value['graph_data'])){ ?>
+                    <?php foreach ($value['graph_data'] as $key1 => $supplier) {?>
+                        <?php if($supplier['count_of_rows'] > 0){ ?>
+                        { label: "<?=$supplier['name'] ?>",  y: <?=$supplier['count_of_rows'] ?>  },
+                    <?php } } } ?>
     				// { label: "grey", y: 15  },
     				// { label: "grey", y: 25  },
     				// { label: "grey",  y: 30  },
@@ -94,43 +87,26 @@ window.onload = function () {
     	chart.render();
 
     <?php } ?>
-    // document.getElementById("printChart").addEventListener("click",function(){
-    //     // contnet.printElement();
-    //     $("#contnet").printElement();
-    // });
-	// var chart = new CanvasJS.Chart("chartContainer1", {
-	// 	title:{
-	// 		text: "My First Chart in CanvasJS"
-	// 	},
-	// 	data: [
-	// 	{
-	// 		// Change type to "doughnut", "line", "splineArea", etc.
-	// 		type: "column",
-	// 		dataPoints: [
-	// 			{ label: "grey",  y: 10  },
-	// 			{ label: "grey", y: 15  },
-	// 			{ label: "grey", y: 25  },
-	// 			{ label: "grey",  y: 30  },
-	// 			{ label: "grey",  y: 28  }
-	// 		]
-	// 	}
-	// 	]
-	// });
-	// chart.render();
+
 
 }
 $('#btnPrint').on('click', function(event) {
         event.preventDefault();
-        html2canvas($('#contnet'), {
+        html2canvas($('#graph_content'), {
             onrendered: function(canvas) {
-                var imgData = canvas.toDataURL('image/jpeg');
+                // var imgData = canvas.toDataURL('image/jpeg');
+                var imgData = canvas.toDataURL();
                 var doc = new jsPDF('landscape');
-                doc.addImage(imgData, 'JPEG', 15, 45, 270, 125);
-                doc.save('download.pdf');
+                // const doc = new jsPDF("l", "mm", "a0"); //  use a4 for smaller page
+
+                // doc.addImage(imgData, 'JPEG', 15, 45, 270, 125);
+                doc.addImage(imgData, 'JPEG', 10, 20, 270, 125);
+                doc.save('grapgh.pdf');
                 return false;
             }
         });
-    });
+
+});
 
 </script>
 <?php $this->load->view('common/footer.php'); ?>
